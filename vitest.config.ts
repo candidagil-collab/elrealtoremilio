5a6ae346d18c7612e13c37024464202b71ec8f7f
candidagil-collab <candida.gil@gmail.com>
2026-03-14 14:51:53 -0500

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+    base: '/elrealtoremilio/',
+  })
   plugins: [react()],
   test: {
     environment: "jsdom",
